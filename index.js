f76698a4a4c26c21b5db43babbcf7d03948e376a
cristianizzo <cristianizzo@me.com>
2018-05-29 02:32:37 +0200

    schema mongoose

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-// const mongoose  = require('mongoose');
 
 module.exports = function(mongoose) {
 
@@ -19,71 +18,3 @@ module.exports = function(mongoose) {
     return Schemas;
 
 };
-
-
-// const adminModel = require('./schema/adminModel');
-// const articleBlogModel = require('./schema/article.blogModel');
-// const articleForumModel = require('./schema/article.forumModel');
-// const billingModel = require('./schema/billingModel');
-// const bundlesModel = require('./schema/bundlesModel');
-// const caseModel = require('./schema/caseModel');
-// const categoryBlogModel = require('./schema/category.blogModel');
-// const categoryForumModel = require('./schema/category.forumModel');
-// const chargeLogModel = require('./schema/chargeLogModel');
-// const comuniModel = require('./schema/comuniModel');
-// const contactModel = require('./schema/contactModel');
-// const couponModel = require('./schema/couponModel');
-// const creditModel = require('./schema/creditModel');
-// const faqLawyerCategoriesModel = require('./schema/faqLawyerCategoriesModel');
-// const faqLawyerModel = require('./schema/faqLawyerModel');
-// const faqModel = require('./schema/faqModel');
-// const feedbackModel = require('./schema/feedbackModel');
-// const invoiceModel = require('./schema/invoiceModel');
-// const landingModel = require('./schema/landingModel');
-// const lawyerModel = require('./schema/lawyerModel');
-// const questionModel = require('./schema/questionModel');
-// const refundModel = require('./schema/refundModel');
-// const regionsModel = require('./schema/regionsModel');
-// const reviewsModel = require('./schema/reviewsModel');
-// const socketModel = require('./schema/socketModel');
-// const specialisationBlogModel = require('./schema/specialisation.blogModel');
-// const specialisationsModel = require('./schema/specialisationsModel');
-// const subCategoryBlogModel = require('./schema/subCategory.blogModel');
-// const forumModel = require('./schema/subCategory.forumModel');
-// const tagBlogModel = require('./schema/tag.blogModel');
-// const userModel = require('./schema/userModel');
-//
-//
-// module.exports = {
-//     adminModel,
-//     articleBlogModel,
-//     articleForumModel,
-//     billingModel,
-//     bundlesModel,
-//     caseModel,
-//     categoryBlogModel,
-//     categoryForumModel,
-//     chargeLogModel,
-//     comuniModel,
-//     contactModel,
-//     couponModel,
-//     creditModel,
-//     faqLawyerCategoriesModel,
-//     faqLawyerModel,
-//     faqModel,
-//     feedbackModel,
-//     invoiceModel,
-//     landingModel,
-//     lawyerModel,
-//     questionModel,
-//     refundModel,
-//     regionsModel,
-//     reviewsModel,
-//     socketModel,
-//     specialisationBlogModel,
-//     specialisationsModel,
-//     subCategoryBlogModel,
-//     forumModel,
-//     tagBlogModel,
-//     userModel
-// }

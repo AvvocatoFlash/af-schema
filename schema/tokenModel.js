f8dac4a1d2ce5266cd917e69c1f3138087cd6557
cristianizzo <cristianizzo@me.com>
2018-10-16 03:04:53 +0200

    token model

--- schema/tokenModel.js
+++ schema/tokenModel.js
@@ -6,8 +6,8 @@ module.exports = (mongoose) => {
 
     let tokenSchema = mongoose.Schema({
         value:   { type: String, require: true },
-        lawyer:  { type: mongoose.Schema.Types.ObjectId, field: "_id", ref: 'lawyer', require: true },
-        type:    { type: String, require: true },
+        lawyer:  { type: mongoose.Schema.Types.ObjectId, field: "_id", ref: 'lawyer' },
+        type:    { type: String },
         updated_at: { type: Date, default: Date.now },
         created_at: { type: Date, default: Date.now }
     });

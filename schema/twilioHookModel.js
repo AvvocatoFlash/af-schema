f9f838303d8605f3ba06af06135fc211c46001f9
cristianizzo <cristianizzo@me.com>
2019-01-15 15:30:01 +0100

    twillio collections stripe collections update lawyer collections

--- schema/twilioHookModel.js
+++ schema/twilioHookModel.js
@@ -5,16 +5,16 @@ module.exports = (mongoose) => {
 
     mongoose.plugin(schema => { schema.options.usePushEach = true });
 
-    let twilioCallbackSchema = mongoose.Schema({
+    let twilioHookSchema = mongoose.Schema({
         json:           { type: Object },
         created_at:    { type: Date, default: Date.now }
     });
 
-    twilioCallbackSchema.pre('save', function(next) {
+    twilioHookSchema.pre('save', function(next) {
         if (!this.isNew) return next();
         if(!this.created_at) this.created_at = Date.now();
         next();
     });
 
-    return  mongoose.model('twilioCallback', twilioCallbackSchema);
+    return  mongoose.model('twilioHook', twilioHookSchema);
 };

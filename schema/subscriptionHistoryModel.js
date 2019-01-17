9be6bfa050ef157f92cf80a2843e521a2acf289d
cristianizzo <cristianizzo@me.com>
2019-01-17 17:31:13 +0100

    new response prop

--- schema/subscriptionHistoryModel.js
+++ schema/subscriptionHistoryModel.js
@@ -1,39 +1,39 @@
-// require('./lawyerModel');
-// require('./comuniModel');
-//
-// module.exports = (mongoose) => {
-//
-//     mongoose.plugin(schema => {
-//         schema.options.usePushEach = true
-//     });
-//
-//     let subscriptionSchema = mongoose.Schema({
-//         status: {type: Boolean, default: false}, //check subscription is enabled
-//         lawyer: {type: mongoose.Schema.Types.ObjectId, field: "_id", ref: 'lawyer', require: true, unique: true},
-//         amount: {type: String},
-//         comuni: [{type: mongoose.Schema.Types.ObjectId, field: "_id", ref: 'comuni'}],
-//         case_qty: {type: String},
-//         start: {type: Date},
-//         expire: {type: Date},
-//         customer: {type: Object},
-//         source: {type: Object},
-//         unsubscribe_at: {type: Date},
-//         updated_at: {type: Date, default: Date.now},
-//         created_at: {type: Date, default: Date.now}
-//     });
-//
-//     subscriptionSchema.pre('save', (next) => {
-//
-//         if (!this.isNew) return next();
-//
-//         if (!this.created_at) {
-//             this.created_at = Date.now();
-//         }
-//
-//         next();
-//     });
-//
-//     return mongoose.model('subscription', subscriptionSchema);
-//
-// };
-//
+require('./lawyerModel');
+require('./comuniModel');
+
+module.exports = (mongoose) => {
+
+    mongoose.plugin(schema => {
+        schema.options.usePushEach = true
+    });
+
+    let subscriptionSchema = mongoose.Schema({
+        // status: {type: Boolean, default: false}, //check subscription is enabled
+        // lawyer: {type: mongoose.Schema.Types.ObjectId, field: "_id", ref: 'lawyer', require: true, unique: true},
+        // amount: {type: String},
+        // comuni: [{type: mongoose.Schema.Types.ObjectId, field: "_id", ref: 'comuni'}],
+        // case_qty: {type: String},
+        // start: {type: Date},
+        // expire: {type: Date},
+        // customer: {type: Object},
+        // source: {type: Object},
+        // unsubscribe_at: {type: Date},
+        // updated_at: {type: Date, default: Date.now},
+        // created_at: {type: Date, default: Date.now}
+    });
+
+    subscriptionSchema.pre('save', (next) => {
+
+        if (!this.isNew) return next();
+
+        if (!this.created_at) {
+            this.created_at = Date.now();
+        }
+
+        next();
+    });
+
+    return mongoose.model('subscription', subscriptionSchema);
+
+};
+

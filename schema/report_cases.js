e1cec47adecaa6936603da56e4712e5edb955bea
cristianizzo <cristianizzo@me.com>
2019-02-11 00:28:46 +0100

    report case table

--- schema/report_cases.js
+++ schema/report_cases.js
@@ -3,24 +3,23 @@ module.exports = (mongoose) => {
     mongoose.plugin(schema => { schema.options.usePushEach = true });
 
     let reportCaseSchema = mongoose.Schema({
-        total_case_sold_today:      { type: String },
-        total_case_approved_today:  { type: String },
-        total_case_received_today:  { type: String },
         total_case:                 { type: String },
         total_case_approved:        { type: String },
         total_case_archived:        { type: String },
         total_case_partners:        { type: String },
+        total_case_approved_today:  { type: String },
+        total_case_received_today:  { type: String },
         total_case_1credit:         { type: String },
-        total_case_2credit:         { type: String },
-        total_case_3credit:         { type: String },
-        total_case_4credit:         { type: String },
-        total_case_more_credit:     { type: String },
+        total_case_2credits:         { type: String },
+        total_case_3credits:         { type: String },
+        total_case_4credits:         { type: String },
+        total_case_more_credits:     { type: String },
         updated_at:    { type: Date, default: Date.now },
         created_at:    { type: Date, default: Date.now }
     });
 
     reportCaseSchema.pre('save', function(next) {
-        if (!this.isNew) return next()
+        if (!this.isNew) return next();
         if(!this.created_at) this.created_at = Date.now();
         if(!this.updated_at) this.updated_at = Date.now();
         next();

8abe566435b13bbd295e64b6377adc8157f09a44
cristianizzo <cristianizzo@me.com>
2019-03-18 18:40:53 +0100

    update report cases

--- schema/report_cases.js
+++ schema/report_cases.js
@@ -3,20 +3,25 @@ module.exports = (mongoose) => {
     mongoose.plugin(schema => { schema.options.usePushEach = true });
 
     let reportCaseSchema = mongoose.Schema({
-        total_case:                 { type: Number },
-        total_case_approved:        { type: Number },
-        total_case_archived:        { type: Number },
-        total_case_partners:        { type: Number },
-        total_case_approved_today:  { type: Number },
-        total_case_received_today:  { type: Number },
-        total_case_1credit:         { type: Number },
-        total_case_2credits:        { type: Number },
-        total_case_3credits:        { type: Number },
-        total_case_4credits:        { type: Number },
-        total_case_more_credits:    { type: Number },
-        total_case_credits:         { type: Number },
-        total_case_credits_spent:   { type: Number },
-        total_case_max_profit:      { type: Number },
+        total_credits_daily_spent:          { type: Number },
+
+        total_case_daily_approved:          { type: Number },
+        total_case_daily_received:          { type: Number },
+        total_case_daily_partners:          { type: Number },
+
+        total_case_monthly:                 { type: Number },
+        total_case_monthly_approved:        { type: Number },
+        total_case_monthly_archived:        { type: Number },
+        total_case_monthly_partners:        { type: Number },
+
+        total_case_monthly_1credit:         { type: Number },
+        total_case_monthly_2credits:        { type: Number },
+        total_case_monthly_3credits:        { type: Number },
+        total_case_monthly_4credits:        { type: Number },
+        total_case_monthly_more_credits:    { type: Number },
+        total_case_monthly_credits_spent:   { type: Number },
+        total_case_monthly_max_profit:      { type: Number },
+
         updated_at:    { type: Date, default: Date.now },
         created_at:    { type: Date, default: Date.now }
     });

debd243cd02f8f4e16f0dc04759f4b4eb7dfca97
cristianizzo <cristianizzo@me.com>
2019-06-26 03:32:09 +0200

    merge specialisation

---
+++ schema/specialisationModel.js
@@ -0,0 +1,51 @@
+const pick = require('lodash.pick');
+
+module.exports = mongoose => {
+
+  mongoose.plugin(schema => {
+    schema.options.usePushEach = true
+  });
+
+  let specialisationSchema = mongoose.Schema({
+    name: {type: String},
+    permalink: {type: String, sparse: true, unique: true, trim: true},
+    name_blog: {type: String},
+    permalink_blog: {type: String, sparse: true, unique: true, trim: true},
+    name_spec: {type: String},
+    permalink_spec: {type: String, sparse: true, unique: true, trim: true},
+    position: {type: Number},
+    isActive: {type: Boolean, default: true},
+    created_at: {type: Date, default: Date.now}
+  });
+
+  specialisationSchema.pre('save', function (next) {
+    if (!this.isNew) return next();
+    if (!this.created_at) this.created_at = Date.now();
+    if (!this.updated_at) this.updated_at = Date.now();
+    next();
+  });
+
+  specialisationSchema.pre('update', function (next) {
+    if (!this.updated_at) this.updated_at = Date.now();
+    next();
+  });
+
+  specialisationSchema.methods = {
+
+    /**
+     * Filter Keys
+     * @return {Object} Custom Keys
+     */
+    filterKeys: function () {
+
+      const obj = this.toObject();
+      const filtered = pick(obj, 'name', 'position', 'permalink');
+
+      return filtered;
+    }
+  };
+
+  return mongoose.model('specialisation', specialisationSchema);
+};
+
+

945dbf044859589b4903473fb9002fa495927976
cristianizzo <cristianizzo@me.com>
2019-10-08 00:23:59 +0800

    edit subscription

--- schema/bundlesModel.js
+++ schema/bundlesModel.js
@@ -16,6 +16,7 @@ module.exports = mongoose => {
     only: [{type: mongoose.Schema.Types.ObjectId, field: "_id", ref: 'lawyer'}],
     price: {type: String},
     credits: {type: Number},
+    card: {type: String},
     expired_at: {type: Date},
     updated_at: {type: Date, default: Date.now},
     created_at: {type: Date, default: Date.now}
